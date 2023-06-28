1acab33dec173b3c9afaabca31dff7b6d9b2327d
matthias.teuschl <wi22b113@technikum-wien.at>
2023-06-28 17:36:29 +0200

    init commit

---
+++ src/main/java/at/fhtw/slmfinalexam/SlmFinalExamApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.slmfinalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmFinalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmFinalExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/slmfinalexam/SlmFinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.slmfinalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmFinalExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
